20b559d1b43ab7214e9d33c3dbe686442e0c46a3
FISERVARG\mcaballaro <matias.caballaro@outlook.com>
2022-09-15 12:35:08 -0300

    Commit prueba

---
+++ src/main/java/com/devforce/devForce/DevForce2023Application.java
@@ -0,0 +1,13 @@
+package com.devforce.devForce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevForce2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevForce2023Application.class, args);
+	}
+
+}
