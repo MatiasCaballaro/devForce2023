f363226f584ef48f36e48fd4823dc4ae87a35e89
Agustin Rubio <agustinrubio2003@gmail.com>
2022-09-20 15:57:43 -0300

    Se creo el usuario DTO

--- src/main/java/com/devforce/devForce/model/dto/UsuarioDTO.java
+++ src/main/java/com/devforce/devForce/model/dto/UsuarioDTO.java
@@ -1,7 +1,28 @@
 package com.devforce.devForce.model.dto;
 
+import com.devforce.devForce.model.entity.Solicitud;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
-
     // TODO: Hacer UsuarioDTO
-
+    private long id;
+    private String nombre;
+    private String apellido;
+    private String username;
+    private String email;
+    private String password;
+    private String phone;
+    private Boolean hasTeams;
+    private String mentorArea;
+    private List<Solicitud> solicitudes;
 }
