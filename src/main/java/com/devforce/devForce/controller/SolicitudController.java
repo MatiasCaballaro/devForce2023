6f3783d4bd788fb55aa302f9ef638df4c512d5b2
mateorubianes <mateorubianes@gmail.com>
2022-09-20 16:29:51 -0300

    Se actualiza solicitudController

--- src/main/java/com/devforce/devForce/controller/SolicitudController.java
+++ src/main/java/com/devforce/devForce/controller/SolicitudController.java
@@ -1,11 +1,30 @@
 package com.devforce.devForce.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.devforce.devForce.model.dto.SolicitudDTO;
+import com.devforce.devForce.model.entity.Usuario;
+import com.devforce.devForce.repository.SolicitudRepository;
+import com.devforce.devForce.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/api/solicitudes")
 public class SolicitudController {
-//    @GetMapping("/solicitudesusuario")
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+    @Autowired
+    SolicitudRepository solicitudRepository;
+
+    @GetMapping("/solicitudesusuario")
+    public List<SolicitudDTO> solicitudesUsuario (@RequestParam long id){
+        //TODO: Cambiar requestParam por el authentication y cambiar el solicitudDTO por un servicio.
+        //return usuarioRepository.findById(id).getSolicitudes().stream().map(SolicitudDTO::new).collect(Collectors.toList());
+        return null;
+    }
+
 //    @GetMapping("/solicitudesmentor")
 //    @GetMapping("/solicitudesadmin")
 //    @PostMapping("/nuevaSolicitud")
