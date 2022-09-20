2fc267313228bb5c12ca1edfb4119ff111b8690c
mateorubianes <mateorubianes@gmail.com>
2022-09-20 15:51:14 -0300

    Se crean AdminController, MentorController, UsuarioController y SolicitudController.

---
+++ src/main/java/com/devforce/devForce/controller/UsuarioController.java
@@ -0,0 +1,12 @@
+package com.devforce.devForce.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/usuario")
+public class UsuarioController {
+//    @GetMapping("/solicitudesusuario")
+//    @GetMapping("/solicitudesmentor")
+//    @GetMapping("/solicitudesadmin")
+//    @PostMapping("/nuevaSolicitud")
+}
