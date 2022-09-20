005a36fd2909a37c99e0f601a7c3fe61150d445c
Ramiro <ramipalacio10@gmail.com>
2022-09-20 16:08:37 -0300

    cambios

--- src/main/java/com/devforce/devForce/model/dto/LicenciaDTO.java
+++ src/main/java/com/devforce/devForce/model/dto/LicenciaDTO.java
@@ -1,7 +1,25 @@
 package com.devforce.devForce.model.dto;
 
+import com.devforce.devForce.model.entity.Solicitud;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+//anotaciones
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class LicenciaDTO {
 
+    private long id;
+    private String serie;
+    private String estado;
+    private LocalDate vencimiento;
+    private String plataforma;
+    private List<Solicitud> solicitudes;
     // TODO: Hacer LicenciaDTO
 
 }
