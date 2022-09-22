ad0500bb5399ad05967bcedb47547fc2cb3acbb4
MateoAmodio <telefonomateo@gmail.com>
2022-09-22 17:19:21 -0300

    Services and implementations

--- src/main/java/com/devforce/devForce/service/AdminService.java
+++ src/main/java/com/devforce/devForce/service/AdminService.java
@@ -1,8 +1,10 @@
 package com.devforce.devForce.service;
 
 import com.devforce.devForce.model.dto.LicenciaDTO;
+import com.devforce.devForce.model.dto.RespuestaDTO;
 import com.devforce.devForce.model.entity.Licencia;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.devforce.devForce.model.entity.Usuario;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -10,4 +12,5 @@ import java.util.List;
 public interface AdminService {
     public List<LicenciaDTO> getLicenciasDTO();
     public List<Licencia> getLicencias();
+    public ResponseEntity<RespuestaDTO> crearUsuario(Usuario usuario);
 }
--- src/main/java/com/devforce/devForce/service/impl/AdminServiceImpl.java
+++ src/main/java/com/devforce/devForce/service/impl/AdminServiceImpl.java
@@ -1,10 +1,15 @@
 package com.devforce.devForce.service.impl;
 
 import com.devforce.devForce.model.dto.LicenciaDTO;
+import com.devforce.devForce.model.dto.RespuestaDTO;
 import com.devforce.devForce.model.entity.Licencia;
+import com.devforce.devForce.model.entity.Usuario;
 import com.devforce.devForce.repository.LicenciaRepository;
+import com.devforce.devForce.repository.UsuarioRepository;
 import com.devforce.devForce.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +20,9 @@ public class AdminServiceImpl implements AdminService {
 
     @Autowired
     LicenciaRepository licenciaRepository;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
 
     public List<Licencia> getLicencias(){
         return licenciaRepository.findAll();
@@ -29,6 +37,24 @@ public class AdminServiceImpl implements AdminService {
         LicenciaDTO licenciaDTO = new LicenciaDTO(licencia.getId(), licencia.getSerie(), licencia.getEstado(),
                 licencia.getVencimiento(), licencia.getPlataforma(), licencia.getSolicitudes());
         return licenciaDTO;
+    }
+
+    public ResponseEntity<RespuestaDTO> crearUsuario(Usuario usuario)
+    {
+
+        // TODO FALTA SECURITY SERVICE
+
+        RespuestaDTO respuestaDTO;
+        if (usuarioRepository.findByUsername(usuario.getUsername()) !=  null) {
+
+            respuestaDTO = new RespuestaDTO(false,"El usuario ya existe",null);
+            return new ResponseEntity<>(respuestaDTO, HttpStatus.FORBIDDEN);
+
+        }
+        usuarioRepository.save(usuario);
+
+        respuestaDTO = new RespuestaDTO(true,"El usuario ha sido creado correctamente",usuario);
+        return new ResponseEntity<>(respuestaDTO,HttpStatus.CREATED);
     }
 
 
