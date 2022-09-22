a611396ba2bd5f0c18830a0e7067b3a30e83211e
santi <santiago.aguilarvieito@gmail.com>
2022-09-22 15:33:18 -0300

    Se termino el @GetMapping("/test/usuarios") del UsuarioController

--- src/main/java/com/devforce/devForce/controller/UsuarioController.java
+++ src/main/java/com/devforce/devForce/controller/UsuarioController.java
@@ -1,17 +1,27 @@
 package com.devforce.devForce.controller;
 import com.devforce.devForce.model.entity.Usuario;
+import com.devforce.devForce.repository.UsuarioRepository;
 import com.devforce.devForce.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/usuario")
 public class UsuarioController {
 
     @Autowired
     UsuarioServiceImpl usuarioServiceImpl;
+    @Autowired
+    UsuarioRepository usuariosRepository;
 
     @PutMapping("/updatedatos")
     public Usuario updateDatos(@RequestBody Usuario usuario) {
         return usuarioServiceImpl.actualizarDatos(usuario);
+    }
+
+    @GetMapping("/test/usuarios")
+    public List<Usuario> testUsuarios() {
+        return usuariosRepository.findAll();
     }
 }
