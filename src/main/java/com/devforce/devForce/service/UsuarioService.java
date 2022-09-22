120422ea5376ff3a1ee5af1b09f110d901a738dd
francojimenezcopati <francojimenezcopati@gmail.com>
2022-09-22 14:28:53 -0300

    Se crea el UsuarioService

---
+++ src/main/java/com/devforce/devForce/service/UsuarioService.java
@@ -0,0 +1,9 @@
+package com.devforce.devForce.service;
+
+import com.devforce.devForce.model.entity.Usuario;
+
+public interface UsuarioService {
+
+    public Usuario updateDatos(Usuario usuario);
+
+}
