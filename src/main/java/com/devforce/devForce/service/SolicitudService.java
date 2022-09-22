8e6a2c2b48d0c27998c73fe0c5c58addde942280
Renzo Villani <renzovillani03@gmail.com>
2022-09-22 15:16:05 -0300

    creación de metodos SolicitudService

--- src/main/java/com/devforce/devForce/service/SolicitudService.java
+++ src/main/java/com/devforce/devForce/service/SolicitudService.java
@@ -1,4 +1,12 @@
 package com.devforce.devForce.service;
 
+import com.devforce.devForce.model.dto.SolicitudDTO;
+import com.devforce.devForce.model.entity.Usuario;
+
+import java.util.List;
+
 public interface SolicitudService {
+    List<SolicitudDTO> getSolicitudesUsuario(Usuario usuario); //TODO: comprobar que sean solicitudes de usuario logeado
+    List<SolicitudDTO> getSolicitudesMentor(Usuario usuario);
+    List<SolicitudDTO> getSolicitudesAdmin(Usuario usuario);
 }
--- src/main/java/com/devforce/devForce/service/impl/SolicitudServiceImpl.java
+++ src/main/java/com/devforce/devForce/service/impl/SolicitudServiceImpl.java
@@ -1,9 +1,36 @@
 package com.devforce.devForce.service.impl;
 
+import com.devforce.devForce.model.dto.SolicitudDTO;
+import com.devforce.devForce.model.entity.Solicitud;
+import com.devforce.devForce.model.entity.Usuario;
+import com.devforce.devForce.repository.SolicitudRepository;
 import com.devforce.devForce.service.SolicitudService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SolicitudServiceImpl implements SolicitudService {
 
+    @Autowired
+    SolicitudRepository solicitudRepository;
+    @Override
+    public List<SolicitudDTO> getSolicitudesUsuario(Usuario usuario) {
+        List<SolicitudDTO> solicitudes = usuario.getSolicitudes().stream().map(SolicitudDTO::new).collect(Collectors.toList());
+        return solicitudes;
+    }
+
+    @Override
+    public List<SolicitudDTO> getSolicitudesMentor(Usuario usuario) {
+        List<SolicitudDTO> solicitudes = this.solicitudRepository.findAll().stream().filter()
+        return solicitudes;
+    }
+
+    @Override
+    public List<SolicitudDTO> getSolicitudesAdmin(Usuario usuario) {
+        List<SolicitudDTO> solicitudes = this.solicitudRepository.findAll().stream().map(SolicitudDTO::new).collect(Collectors.toList());
+        return solicitudes;
+    }
 }
