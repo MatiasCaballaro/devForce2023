4c76e0d5f9e38b19e9efab7e3f55c18e58d1e5cc
Agustin Rubio <agustinrubio2003@gmail.com>
2022-09-22 17:36:04 -0300

    Se creo la interfaz de Solicitud Service Se creo el impl de solicitud Service Se creo el servicio para crear solicitud Se creo servicio para devolver solicitudes

--- src/main/java/com/devforce/devForce/service/SolicitudService.java
+++ src/main/java/com/devforce/devForce/service/SolicitudService.java
@@ -1,12 +1,17 @@
 package com.devforce.devForce.service;
 
 import com.devforce.devForce.model.dto.SolicitudDTO;
+import com.devforce.devForce.model.entity.Solicitud;
 import com.devforce.devForce.model.entity.Usuario;
 
 import java.util.List;
 
 public interface SolicitudService {
-    List<SolicitudDTO> getSolicitudesUsuario(Usuario usuario); //TODO: comprobar que sean solicitudes de usuario logeado
-    List<SolicitudDTO> getSolicitudesMentor(Usuario usuario);
-    List<SolicitudDTO> getSolicitudesAdmin(Usuario usuario);
+    //TODO: REEMPLAZAR EL Usuario usuario POR AUTHENTICATION CUANDO LE INTEGREMOS EL SECURITY
+    public Solicitud crearSolicitud(Solicitud solicitud, Usuario usuario);
+    public SolicitudDTO crearSolicitudDTO(Solicitud solicitud);
+    public List<SolicitudDTO> getSolicitudesUsuario(Usuario usuario);
+    public List<SolicitudDTO> getSolicitudesMentor(Usuario usuario);
+    public List<SolicitudDTO> getSolicitudesAdmin(Usuario usuario);
+    public Solicitud actualizarSolicitud(Solicitud solicitud, Usuario usuario);
 }
