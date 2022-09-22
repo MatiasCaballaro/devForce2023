d9e26d5db736cedf48d625ee0124e7b8c55aeafc
MateoAmodio <telefonomateo@gmail.com>
2022-09-22 17:10:23 -0300

    Services and implementations

--- src/main/java/com/devforce/devForce/controller/AdminController.java
+++ src/main/java/com/devforce/devForce/controller/AdminController.java
@@ -3,6 +3,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @RequestMapping("/api/admin")
 public class AdminController {
 //    @PostMapping("/registrarUsuario")
--- src/main/java/com/devforce/devForce/service/AdminService.java
+++ src/main/java/com/devforce/devForce/service/AdminService.java
@@ -1,4 +1,13 @@
 package com.devforce.devForce.service;
 
+import com.devforce.devForce.model.dto.LicenciaDTO;
+import com.devforce.devForce.model.entity.Licencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
 public interface AdminService {
+    public List<LicenciaDTO> getLicenciasDTO();
+    public List<Licencia> getLicencias();
 }
