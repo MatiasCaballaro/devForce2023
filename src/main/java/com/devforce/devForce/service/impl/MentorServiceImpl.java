608b822441666c791fc736f23f292cdc1b0903d9
Valentin Barallobre <valenn.t.bara@gmail.com>
2022-09-22 17:51:52 -0300

    Creamos MentorService y MentorServiceImpl. Colocamos los metodos de aceptar, rechazar y devolver solicitud. A su vez agregamos un metodo de verificacion encargado de reducir la repeticion de codigo.

--- src/main/java/com/devforce/devForce/service/impl/MentorServiceImpl.java
+++ src/main/java/com/devforce/devForce/service/impl/MentorServiceImpl.java
@@ -7,7 +7,9 @@ import com.devforce.devForce.service.MentorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MentorServiceImpl implements MentorService {
 
     @Autowired
