f955dcc9f8b906a44fba92182fc413f56e382557
Santino Scannapieco <sanscannapieco@pioix.edu.ar>
2022-09-23 16:01:52 -0300

    Se avanzo el @GetMapping("/test/usuariosDTO") del UsuarioController

--- src/main/java/com/devforce/devForce/controller/AdminController.java
+++ src/main/java/com/devforce/devForce/controller/AdminController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api")
 public class AdminController {
     @Autowired
     AdminServiceImpl adminServiceImpl;
--- src/main/java/com/devforce/devForce/controller/SolicitudController.java
+++ src/main/java/com/devforce/devForce/controller/SolicitudController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/solicitudes")
+@RequestMapping("/api")
 public class SolicitudController {
 
     @Autowired
