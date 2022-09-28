f33bafd25348c39eae0423b03c5c9397353b2842
MateoAmodio <telefonomateo@gmail.com>
2022-09-28 15:40:22 -0300

    Services and implementations

--- src/main/java/com/devforce/devForce/repository/LicenciaRepository.java
+++ src/main/java/com/devforce/devForce/repository/LicenciaRepository.java
@@ -3,5 +3,10 @@ package com.devforce.devForce.repository;
 import com.devforce.devForce.model.entity.Licencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LicenciaRepository extends JpaRepository<Licencia, Long> {
+
+    List<Licencia> findByTipoAndEstado (String tipo, String estado);
+
 }
